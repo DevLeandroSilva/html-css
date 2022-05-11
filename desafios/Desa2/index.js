c979fff3b9878b776489ee9fe23d33fa4615d47b
DevLeandroSilva <77212529+DevLeandroSilva@users.noreply.github.com>
2022-05-11 12:29:05 -0300

    Criação da pasta desafios propostos pelo Professor Gustavo Guanabara e atualização das páginas criadas

---
+++ desafios/Desa2/index.js
@@ -0,0 +1,8 @@
+var resultado = parseInt(num1) + parseInt(num2);
+var resultado = num1 +num2;
+var num1 = 'num1';
+function result() {
+    var num1 = document.querySelector(".num1").value;
+    var num2 = document.querySelector(".num2").value;
+}
+document.querySelector(".resultado").innerHTML = resultado;
